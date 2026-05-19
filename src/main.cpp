735833aaaf50e4080ad210a69e025ca91fa92343
Charles Benson <charlestikouka@icloud.com>
2026-05-19 23:13:19 +1200

    removed testing code from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,15 @@
 #include "../include/bobbersQueue.h"
-#include <stdio.h>
-#include "../include/bobbersCalcInput.h"
-// #include <Arduino.h>
+#include <Arduino.h>
 
 /* MAIN FUNCTIONS START */
 
-// void setup() {
-// 	Queue queue;
-// 	pushQueueTail(&queue, 'c');
-// 	printQueue(&queue);
-// }
-//
-// void loop() {
-// }
-int main() {
-    printf("%c \n", operand_input());
-    printf("%c \n", operator_input());
+void setup() {
+	Queue queue;
+	pushQueueTail(&queue, 'c');
+	printQueue(&queue);
+}
+
+void loop() {
 }
 
 /* MAIN FUNCTIONS END */
