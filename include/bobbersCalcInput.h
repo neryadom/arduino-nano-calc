d9e41549f8c55b6d6463f7da4c35ea14c1b06464
Charles Benson <charlestikouka@icloud.com>
2026-05-19 23:08:13 +1200

    added calculator input logic

--- include/bobbersCalcInput.h
+++ include/bobbersCalcInput.h
@@ -1,8 +1,22 @@
-//
-// Created by Charles Benson on 19/05/2026.
-//
+#pragma once
 
-#ifndef ARDUINO_NANO_CALC_BOBBERSCALCINPUT_H
-#define ARDUINO_NANO_CALC_BOBBERSCALCINPUT_H
+/* Get input from button 1. For specifying operands and operators */
+int button_1_input();
 
-#endif //ARDUINO_NANO_CALC_BOBBERSCALCINPUT_H
+/*
+ * Get operand input
+ * Number of button presses corresponds is the operand (domain restricted to integers greater to or equal to 1)
+ */
+char operand_input();
+
+/* Get operator input, also includes brackets.
+ * Number of presses: operator
+ * 0: +
+ * 1: -
+ * 2: *
+ * 3: /
+ * 4: (
+ * 5: )
+ */
+char operator_input();
+
