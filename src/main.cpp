d9ba5e280675398d495d2bca04a0b196eec6b9ff
neryadom <henrichemla@outlook.com>
2026-05-19 21:29:17 +1200

    Cleaning main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,15 @@
-#include <stdio.h>
 #include "../include/bobbersQueue.h"
 #include <Arduino.h>
 
-constexpr int ledPin = 13;
-
 /* MAIN FUNCTIONS START */
 
-
-
-// int main() {
-// 	Queue queue;
-// 	resetQueue(&queue);
-// 	printQueue(&queue);
-// 	pushQueueTail(&queue, '5');
-// 	pushQueueTail(&queue, '*');
-// 	pushQueueTail(&queue, '3');
-// 	pushQueueTail(&queue, '=');
-// 	printQueue(&queue);
-// 	char buffer;
-// 	popQueueHead(&queue, &buffer);
-// 	printf("Popped from head: %c", buffer);
-// 	printQueue(&queue);
-// 	popQueueHead(&queue, &buffer);
-// 	printf("Popped from head: %c", buffer);
-// 	printQueue(&queue);
-// 	popQueueHead(&queue, &buffer);
-// 	printf("Popped from head: %c", buffer);
-// 	printQueue(&queue);
-// 	popQueueHead(&queue, &buffer);
-// 	printf("Popped from head: %c", buffer);
-// 	printQueue(&queue);
-// 	popQueueHead(&queue, &buffer);
-// 	printf("Popped from head: %c", buffer);
-// 	printQueue(&queue);
-// 	return 0;
-// }
-
 void setup() {
-	pinMode(13, OUTPUT);
-}
-void loop() {
-	digitalWrite(ledPin, HIGH);
-	delay(100);
-	digitalWrite(ledPin, LOW);
-	delay(100);
+	Queue queue;
+	pushQueueTail(&queue, 'c');
+	printQueue(&queue);
 }
 
+void loop() {
+}
 
 /* MAIN FUNCTIONS END */
